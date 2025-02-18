8653424f5c051e6e385660c4e465c65488edb149
shadmanadman <adman.shadman@gmail.com>
2025-02-18 21:18:38 +0330

    Introduce KmpWebView for Android
    This commit introduces the `KmpWebView` composable function for Android.
    - Implements `KmpWebView` for Android.
    - Creates a new `KmpWebView.android.kt` file.
    - Updates `KmpWebView.kt` file for common code.
    - Updates `KmpWebViewScreen.kt` file for preview.
    - Adds `AppContext` to initialize WebView.

--- composeApp/src/commonMain/kotlin/org/adman/kmp/webview/KmpWebView.kt
+++ composeApp/src/commonMain/kotlin/org/adman/kmp/webview/KmpWebView.kt
@@ -1,7 +1,8 @@
 package org.adman.kmp.webview
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 typealias UrlOrHtmlContent = String
 @Composable
-expect fun KmpWebView(urlOrHtmlContent: UrlOrHtmlContent, isLoading:(isLoading:Boolean)->Unit, onUrlClicked:(url:String)->Unit)
+expect fun KmpWebView(modifier: Modifier, urlOrHtmlContent: UrlOrHtmlContent, isLoading:(isLoading:Boolean)->Unit, onUrlClicked:(url:String)->Unit)
