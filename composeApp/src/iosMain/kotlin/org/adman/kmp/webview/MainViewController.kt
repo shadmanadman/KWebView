f276f60bbbf0405bad610b107fe06844ce492fbd
shadmanadman <adman.shadman@gmail.com>
2025-06-28 13:21:16 +0330

    Remove unused resources

--- composeApp/src/iosMain/kotlin/org/adman/kmp/webview/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package org.adman.kmp.webview
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { KmpWebViewScreen(url = "https://www.google.com/") }
